432f24838904669836129b60eabf3836c9c71c3d
Henrique Medeiros <henrique.medeiros@alunos.sc.senac.br>
2022-08-26 21:04:02 -0300

    Nova classe Carro adicionada e nova impressão

---
+++ src/githubaula1/Carro.java
@@ -0,0 +1,12 @@
+
+package githubaula1;
+
+import java.util.Scanner;
+
+public class Carro {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner (System.in);
+        
+    }
+    
+}
